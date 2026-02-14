d446f8265a5348126337df32822b1e1340a6f470
Valinthieth <ishfaqmcb12@gmail.com>
2026-02-14 16:34:47 -0500

    chore: restructuing, delting api

--- django-backend/config/django/base.py
+++ django-backend/config/django/base.py
@@ -37,7 +37,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'api',
     'rest_framework',
 ]
 
@@ -121,4 +120,3 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
-AUTH_USER_MODEL = 'api.User'
