651b897cf3ab532f5daa53e171d0abbc337ab78e
Valinthieth <ishfaqmcb12@gmail.com>
2026-02-23 22:44:33 -0500

    feat: models for orgnizations and members

--- django-backend/api/models/accounts.py
+++ django-backend/api/models/accounts.py
@@ -3,5 +3,5 @@ from django.contrib.auth.models import User
 
 class Member(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    first_name = models.CharField(max_length=255)
-    last_name = models.CharField(max_length=255)
+    first_name = models.CharField(max_length=150)
+    last_name = models.CharField(max_length=150)
