8b4b37704fc8cde4cf1ec101207ce86414c2910a
Valinthieth <ishfaqmcb12@gmail.com>
2026-02-28 15:00:35 -0500

    fix: user should be a foreign key for institutions

--- django-backend/api/models/institution.py
+++ django-backend/api/models/institution.py
@@ -8,7 +8,7 @@ class InstitutionId(models.Model):
     code = models.CharField(max_length=6, unique=True)
     used = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
 
     class Meta:
         ordering = ["created_at"]
