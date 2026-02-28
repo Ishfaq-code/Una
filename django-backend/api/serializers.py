60b5b03a7c34f83a8cbad216d613fa90d3c444e7
Valinthieth <ishfaqmcb12@gmail.com>
2026-02-28 15:56:53 -0500

    feat: institution serializer

---
+++ django-backend/api/serializers.py
@@ -0,0 +1,16 @@
+from rest_framework import serializers
+from api.models.institution import InstitutionId
+
+class InstitutionSerializer(serializers.Serializer):
+    user_first_name = serializers.CharField(source='user.first_name')
+    user_last_name = serializers.CharField(source='user.last_name')
+    
+    class Meta:
+        model = InstitutionId
+        fields = (
+            'id',
+            'code',
+            'created_at',
+            'user_first_name',
+            'user_last_name'
+        )
