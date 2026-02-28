9131e1d8e8416c20f284eaed6437a003dd61528f
Valinthieth <ishfaqmcb12@gmail.com>
2026-02-28 16:28:42 -0500

    fix: added readonly field for serializers + fixed formatting

--- django-backend/api/serializers.py
+++ django-backend/api/serializers.py
@@ -4,13 +4,8 @@ from api.models.institution import InstitutionId
 class InstitutionSerializer(serializers.Serializer):
     user_first_name = serializers.CharField(source='user.first_name')
     user_last_name = serializers.CharField(source='user.last_name')
-    
+
     class Meta:
         model = InstitutionId
-        fields = (
-            'id',
-            'code',
-            'created_at',
-            'user_first_name',
-            'user_last_name'
-        )
+        fields = ['id','code','created_at','user_first_name','user_last_name']
+        read_only_fields = ["id", "code", "used", "created_at", "user_first_name", "user_last_name"]
