dece1c3b8d28bcf820492fd91f87aebb6b0a66bd
Valinthieth <ishfaqmcb12@gmail.com>
2026-02-28 14:12:38 -0500

    feat: instituion ids are linked with user to keep track of who is creating what

--- django-backend/api/models/institution.py
+++ django-backend/api/models/institution.py
@@ -1,9 +1,14 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
+
 
 
 class InstitutionId(models.Model):
     code = models.CharField(max_length=6, unique=True)
     used = models.BooleanField(default=False)
-    created_at = models.DateTimeField()
-    
+    created_at = models.DateTimeField(auto_now_add=True)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    class Meta:
+        ordering = ["created_at"]
