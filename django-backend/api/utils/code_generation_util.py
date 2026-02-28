6b22593230c6f31fb1680a89769a619b0d5070c5
Valinthieth <ishfaqmcb12@gmail.com>
2026-02-28 16:25:48 -0500

    utility function for creating instituion code

---
+++ django-backend/api/utils/code_generation_util.py
@@ -0,0 +1,7 @@
+import secrets
+import string
+
+ALPHANUM = string.ascii_uppercase + string.digits
+
+def generate_institution_code(length: int = 6) -> str:
+    return ''.join(secrets.choice(ALPHANUM) for _ in range(length))
