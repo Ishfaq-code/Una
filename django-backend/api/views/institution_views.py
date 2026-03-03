9e66157adaad0e452a2e16ebb6df8f0d4fad694f
Valinthieth <ishfaqmcb12@gmail.com>
2026-03-02 21:34:16 -0500

    feat: get latest institituion code

--- django-backend/api/views/institution_views.py
+++ django-backend/api/views/institution_views.py
@@ -2,8 +2,22 @@ from django.db import IntegrityError, transaction
 from rest_framework import generics, permissions
 from api.models.institution import InstitutionId
 from api.serializers import InstitutionSerializer
+from rest_framework.exceptions import NotFound
+
 
 class InstitutionIdCreateView(generics.CreateAPIView):
     serializer_class = InstitutionSerializer
     permission_classes = [permissions.IsAdminUser]
  
+class GetLatestInstitution(generics.RetrieveAPIView):
+    serializer_class = InstitutionSerializer
+    permission_classes = [permissions.IsAdminUser]
+
+    def get_object(self):
+        institution_code = (InstitutionId.objects
+               .filter(user=self.request.user, used=False)
+               .order_by("-created_at")
+               .first())
+        if not institution_code:
+            raise NotFound("No avaliable institution codes, please create one")
+        return institution_code
