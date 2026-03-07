d1c13dda2f9c1f77b3f834c7c5a8360ef61165fd
Valinthieth <ishfaqmcb12@gmail.com>
2026-03-07 10:05:29 -0500

    feat: user authentication done by email

--- django-backend/api/models/accounts.py
+++ django-backend/api/models/accounts.py
@@ -1,7 +1,8 @@
+from django.conf import settings
 from django.db import models
-from django.contrib.auth.models import User
+
 
 class Member(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     first_name = models.CharField(max_length=150)
     last_name = models.CharField(max_length=150)
--- django-backend/api/models/institution.py
+++ django-backend/api/models/institution.py
@@ -1,14 +1,11 @@
+from django.conf import settings
 from django.db import models
-from django.utils import timezone
-from django.contrib.auth.models import User
-
-
 
 class InstitutionId(models.Model):
     code = models.CharField(max_length=6, unique=True)
     used = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
 
     class Meta:
         ordering = ["created_at"]
