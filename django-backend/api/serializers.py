8455ada05326fb42e567e145535272d23f6a5486
Ariel Fevry <arielfevry@gmail.com>
2026-03-11 00:14:58 -0400

    feat: serializer to create clubs in database

--- django-backend/api/serializers.py
+++ django-backend/api/serializers.py
@@ -1,6 +1,8 @@
 from rest_framework import serializers
 from api.models.institution import InstitutionId
-from api.utils.code_generation_util import generate_institution_code
+from api.models.organizations import Organizations, OrganizationCode, Memberships
+from api.utils.code_generation_util import generate_institution_code, generate_organization_code
+from api.utils.hash_util import encode_code
 from django.db import IntegrityError, transaction
 
 
@@ -25,4 +27,52 @@ class InstitutionSerializer(serializers.ModelSerializer):
                 )
         except IntegrityError:
             raise IntegrityError('Failed creating a new institution code, try again!')
-            
+
+
+class OrganizationSerializer(serializers.ModelSerializer):
+    # data needed from other models (relational objects) not user inputs 
+    owner_first_name = serializers.CharField(source="owner.first_name", read_only=True)
+    owner_last_name = serializers.CharField(source="owner.last_name", read_only=True)
+    institution_id = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = Organizations
+        fields = ['id', 'institution_id', 'created_at', 'updated_at', 'name', 'description','owner_first_name', 'owner_last_name'] # display
+        read_only_fields = ['id', 'created_at', 'updated_at', 'owner_first_name', 'owner_last_name'] # not user input
+
+    def create(self, validated_data):  
+        owner = Member.objects.get(user=self.context['request'].user)
+        try:
+            with transaction.atomic():
+                institution_id = validated_data.pop('institution_id') # pop user inputed code
+                institution = InstitutionId.objects.get(code__exact=institution_id, used=False) #check if code exists/hasnt been used
+                org_code = encode_code(generate_organization_code()) 
+
+                institution.used=True #consume code
+                institution.save()
+
+                organization = Organizations.objects.create(    
+                    owner=owner,
+                    institution_id=institution,
+                    **validated_data
+                )
+
+                OrganizationCode.objects.create(
+                    code=org_code,
+                    organization=organization
+                )
+
+                Memberships.objects.create(
+                    member=owner,
+                    organization=organization,
+                    status=1,
+                    role=Memberships.ClubRole.ADMIN
+                )
+                
+                return Memberships.objects.filter(member=owner)
+
+        except IntegrityError:
+            raise IntegrityError('Failed creating a new organization, try again!')
+        except InstitutionId.DoesNotExist:
+            raise serializers.ValidationError('Instituon code is invalid, try again!')
+           
