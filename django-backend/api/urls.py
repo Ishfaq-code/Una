42bf6bcac6154cf581e0fa85b9f22a3f87b01a49
Valinthieth <ishfaqmcb12@gmail.com>
2026-03-16 19:24:45 -0400

    feat: retrive current user route and routes overhaul

--- django-backend/api/urls.py
+++ django-backend/api/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from api.views.institution_views import InstitutionIdCreateView, GetLatestInstitution
-from api.views.user_views import RegisterUser
+from api.views.user_views import RegisterUser, CurrentUser
 
 urlpatterns = [
-    path("institutions/", InstitutionIdCreateView.as_view(), name="institutionid-create"),
-    path("institutions/latest/", GetLatestInstitution.as_view(), name="institutionid-latest"),
-    path("auth/register/", RegisterUser.as_view(), name="register-user")
+    path("api/institutions/", InstitutionIdCreateView.as_view(), name="institutionid-create"),
+    path("api/institutions/latest/", GetLatestInstitution.as_view(), name="institutionid-latest"),
+    path("api/register/", RegisterUser.as_view(), name="register-user"),
+    path("api/users/", CurrentUser.as_view(), name="fetech-current-user")
 ]
--- django-backend/api/views/user_views.py
+++ django-backend/api/views/user_views.py
@@ -5,3 +5,10 @@ from api.serializers import UserSerializer
 class RegisterUser(generics.CreateAPIView):
     serializer_class = UserSerializer
     permission_classes = [permissions.AllowAny]
+
+class CurrentUser(generics.RetrieveAPIView):
+    serializer_class = UserSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_object(self):
+        return self.request.user
