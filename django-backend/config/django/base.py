267549c85d4b96206e096332821318a1f13d605f
Valinthieth <ishfaqmcb12@gmail.com>
2026-03-20 19:58:43 -0400

    chore: add corsheaders to avoid cors issues

--- django-backend/config/django/base.py
+++ django-backend/config/django/base.py
@@ -47,11 +47,13 @@ INSTALLED_APPS = [
     'api',
     'rest_framework',
     'users',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -92,6 +94,11 @@ DATABASES = {
         'PORT': env('DB_PORT'),
     }
 }
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173"
+]
+CORS_ALLOW_CREDENTIALS = True
 
 
 # Password validation
